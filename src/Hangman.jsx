610be380eb625f70ae8fdbfb1a67963b8a53f376
Meenakshi Dhanani <meena24dhanani@gmail.com>
2020-10-11 16:21:32 +0530

    Build hangman - reveal letters on guess using state hook

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Hangman() {
 	const word = "HANGMAN";
     const alphabets = ["A", "B", "C", "D", "E", "F", "G",
         "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
         "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	const [correctGuesses, setCorrectGuesses] = useState([])	
 
+	const maskedWord = word.split('').map(letter => 
+	correctGuesses.includes(letter) ? letter : "_").join(" ");
+	
 	return 	<div>
-			<p>{word.split("").fill("_").join(" ")}</p>
+			<p>{maskedWord}</p>
 			{alphabets
 			.map((alphabet, index) => 
-			<button key={index}>{alphabet}</button>)}
+			<button key={index} onClick={() => {
+                if (word.includes(alphabet)) {
+                    setCorrectGuesses([...correctGuesses, alphabet])
+                }
+            }}>{alphabet}</button>)}
+			{!maskedWord.includes("_") && <p>You won!</p>}
 			</div>
 }
