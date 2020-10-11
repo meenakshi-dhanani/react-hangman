5ddf7f4d2910d640062e246af3bc7f50e0237c8a
Meenakshi Dhanani <meena24dhanani@gmail.com>
2020-10-11 16:18:00 +0530

    Set up basic layout for Hangman

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Hangman() {
-    return <></>;
+	const word = "HANGMAN";
+    const alphabets = ["A", "B", "C", "D", "E", "F", "G",
+        "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+	return 	<div>
+			<p>{word.split("").fill("_").join(" ")}</p>
+			{alphabets
+			.map((alphabet, index) => 
+			<button key={index}>{alphabet}</button>)}
+			</div>
 }
