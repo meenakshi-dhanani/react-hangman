9c1ec4f068eb18e0c521622ea385dd49e543be96
Meenakshi Dhanani <meena24dhanani@gmail.com>
2020-10-11 16:23:01 +0530

    Add timeout - 2 min if word is not guessed using the effect hook

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -1,24 +1,34 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Hangman() {
-	const word = "HANGMAN";
+export default function Hangman({duration = 120000}) {
+    const word = "Hangman".toUpperCase();
     const alphabets = ["A", "B", "C", "D", "E", "F", "G",
         "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
         "S", "T", "U", "V", "W", "X", "Y", "Z"];
-	const [correctGuesses, setCorrectGuesses] = useState([])	
+    const [correctGuesses, setCorrectGuesses] = useState([])
+    const [timeUp, setTimeUp] = useState(false);
 
-	const maskedWord = word.split('').map(letter => 
-	correctGuesses.includes(letter) ? letter : "_").join(" ");
-	
-	return 	<div>
-			<p>{maskedWord}</p>
-			{alphabets
-			.map((alphabet, index) => 
-			<button key={index} onClick={() => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setTimeUp(true);
+        }, duration);
+
+        return () => clearTimeout(timeout);
+    }, [])
+
+
+    const maskedWord = word.split('').map(letter => correctGuesses.includes(letter) ? letter : "_").join(" ");
+    return (
+        <div>
+            <p>{maskedWord}</p>
+            {alphabets.map((alphabet, index) => <button key={index} onClick={() => {
                 if (word.includes(alphabet)) {
                     setCorrectGuesses([...correctGuesses, alphabet])
                 }
             }}>{alphabet}</button>)}
-			{!maskedWord.includes("_") && <p>You won!</p>}
-			</div>
+			{timeUp ? 
+			<p>You lost!</p> : 
+			!maskedWord.includes("_") &&  <p>You won!</p>}
+        </div>
+    );
 }
